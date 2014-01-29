02f040cecf406d28de89248597e8761887a0a8c3
Tito Agudelo <titoagudelo@Administradors-MacBook-Pro.local>
2014-01-29 15:58:15 -0500

    config fab

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
-LOGFILE=/home/admin/Workspace/pando/pando/logs/gunicorn.log
+LOGFILE=/home/admin/Workspace/pandos/pando/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=1
 # user/group to run as
 USER=root
 GROUP=root
 ADDRESS=127.0.0.1:8000
-cd /home/admin/Workspace/pando/pando
-source /home/admin/Workspace/pando/bin/activate
+cd /home/admin/Workspace/pandos/pando
+source /home/admin/Workspace/pandos/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn_django pando -w $NUM_WORKERS --bind=$ADDRESS \
   --user=$USER --group=$GROUP --log-level=debug \
